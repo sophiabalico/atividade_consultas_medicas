cfd2ae9db2ee9be2b18feae2d64b0ae48c75af73
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-11-12 21:47:57 -0300

    update: correção de erro

--- sophiabalico.sql
+++ sophiabalico.sql
@@ -19,7 +19,7 @@ CREATE TABLE consultas (
     id_paciente INT,
     id_medico INT,
     data_consulta DATE,
-    horario_consulta VARCHAR(5),
+    horario_consulta TIME,
     FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
     FOREIGN KEY (id_medico) REFERENCES medicos(id_medico)
 );
